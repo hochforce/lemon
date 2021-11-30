e7f49c31a502158f6d8df9d445e1319edc1c0225
Hugo Hoch <hugohoch2013@gmail.com>
2021-11-29 23:10:36 -0300

    Fixing bugs on registration page

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Content, SInput, Label } from './styles.js'
 
-const Input = ({label, type, value, onChange}) => {
+const Input = ({label, type, value, onChange, required}) => {
   return (
     <Container>
       <Label>{label}</Label>
       <Content>
-        <SInput type={type} value={value} onChange={onChange}/>
+        <SInput type={type} value={value} onChange={onChange} required={required}/>
       </Content>
     </Container>
   )
