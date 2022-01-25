6636efde20df179f5eeb4b5b7843967c642f45de
Hugo Hoch <hugohoch2013@gmail.com>
2022-01-25 00:02:14 -0300

    Fixing bug on branch feature/certificate

--- backend/src/controllers/ParticipanteController.ts
+++ backend/src/controllers/ParticipanteController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, getConnection } from 'typeorm';
 import { Participante } from '../models/Participante';
+import bcrypt from 'bcryptjs';
+
 
 class ParticipanteController {
   async create(request: Request, response: Response) {
@@ -29,16 +31,41 @@ class ParticipanteController {
   async search(request: Request, response: Response) {
     const participanteSearch = await getRepository(Participante)
       .createQueryBuilder("participantes")
-      .where("cpf = :cpf", {cpf: request.params.cpf})
+      .where("cpf = :cpf", { cpf: request.params.cpf })
       .getOne();
     return response.json(participanteSearch);
   }
   async searchById(request: Request, response: Response) {
     const participanteSearch = await getRepository(Participante)
       .createQueryBuilder("participantes")
-      .where("id = :id", {id: request.params.id})
+      .where("id = :id", { id: request.params.id })
       .getOne();
     return response.json(participanteSearch);
+  }
+  async update(request: Request, response: Response) {
+    const search = await getRepository(Participante)
+      .createQueryBuilder("participantes")
+      .where("id = :id", { id: request.params.id })
+      .getOne();
+    var {
+      nome,
+      sobrenome,
+      campus_instituicao,
+      password
+    } = request.body;
+    request.body.password = bcrypt.hashSync(request.body.password, 8);
+    await getConnection()
+      .createQueryBuilder()
+      .update(Participante)
+      .set({
+        nome: request.body.nome ? request.body.nome : search.nome,
+        sobrenome: request.body.sobrenome ? request.body.sobrenome : search.sobrenome,
+        campus_instituicao: request.body.campus_instituicao ? request.body.campus_instituicao : search.campus_instituicao,
+        password: request.body.password ? request.body.password : search.password
+      })
+      .where("id = :id", { id: request.params.id })
+      .execute();
+    return response.json(response.status);
   }
 
 }
