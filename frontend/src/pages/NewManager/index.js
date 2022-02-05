e5887bf212f789d65689347210c04a5a7553d85c
Hugo Hoch <hugohoch2013@gmail.com>
2022-02-05 00:38:34 -0300

    Ideal functions

--- frontend/src/pages/NewManager/index.js
+++ frontend/src/pages/NewManager/index.js
@@ -1,11 +1,24 @@
 import React, { useRef, useState } from 'react';
-import * as Yup from 'yup';
-import { Form } from '@unform/web';
-import { Input } from '../../components/Form/input';
+import { Container, Content, P, View, ViewInputs, ViewError } from "./styles.js";
 import { api } from '../../services/api';
+import { ModalConfirm } from './../../components/ModalConfirm/index';
+import Input from "../../components/Input";
+import { Header } from './../../components/Header/index';
+import { Link } from 'react-router-dom';
+import { Button } from "../../components/Button";
+import { validaCpf } from '../../components/CPFValidate';
 
 export default function NewManager({ history }) {
 
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+
+    setShowModal(prev => !prev)
+  }
+  const [status, setStatus] = useState({
+    type: '',
+    mensagem: ''
+  });
   const [nome, setNome] = useState('');
   const [sobrenome, setSobrenome] = useState('');
   const [cpf, setCpf] = useState('');
@@ -15,142 +28,171 @@ export default function NewManager({ history }) {
   const [telefone, setTelefone] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [repeatPass, setRepeatPass] = useState('');
 
-  const formRef = useRef(null);
 
-  async function handleSubmit(data, { reset }, event) {
+  async function addUser(event) {
     event.preventDefault();
+    if (validate()) {
+      if (!validaCpf(cpf)) {
+        return setStatus({ type: 'error', mensagem: 'Erro: Insira um CPF válido. (Digite somente os números)!' });
+      } else {
+        try {
+          //Salvando na tabela contatos
+          const saveContact = await api.post('/contatos', {
+            telefone,
+            email,
+          });
 
-    try {
-      const schema = Yup.object().shape({
-        nome: Yup.string().required('O nome é obrigatório'),
-        sobrenome: Yup.string().required('O sobrenome é obrigatório'),
-        cpf: Yup.string().required('O CPF é obrigatório'),
-        titulacao: Yup.string().required('A titulação é obrigatória'),
-        cargo: Yup.string().required('O cargo é obrigatório'),
-        campus_instituicao: Yup.string().required('O sobrenome é obrigatório'),
-        telefone: Yup.string().required('O telefone é obrigatório'),
-        password: Yup.string().required('É necessário criar uma senha')
-      })
-      await schema.validate(data, {
-        abortEarly: false,
-      })
+          //Pegando o id_contact
+          const id_contato = saveContact.data.id;
+          console.log(id_contato);
 
-      console.log(data);
 
-      //Salvando na tabela contatos
-      const saveContact = await api.post('/contatos', {
-        telefone,
-        email,
-      });
+          //Salvando na tabela organizadores
+          await api.post('/organizadores', {
+            id_contato,
+            nome,
+            sobrenome,
+            cpf,
+            campus_instituicao,
+            titulacao,
+            cargo,
+            password
+          });
 
-      //Pegando o id_contact
-      const id_contato = saveContact.data.id;
-      console.log(id_contato);
-      
+          const saveManager = await api.post('/user-auth', {
+            cpf,
+            password,
+            "tipo": "organizador"
+          })
 
-      //Salvando na tabela organizadores
-      await api.post('/organizadores', {
-        id_contato,
-        nome,
-        sobrenome,
-        cpf,
-        campus_instituicao,
-        titulacao,
-        cargo,
-        password
-      });
-
-      await api.post('/user-auth', {
-        cpf,
-        password,
-        "tipo": "organizador"
-      })
-
-      // const { id } = saveManager.data;
-      // localStorage.setItem('organizador', id);
-
-      formRef.current.setErrors({});
-      reset();
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errorMessages = {};
-
-        err.inner.forEach(error => {
-          errorMessages[error.path] = error.message;
-        })
-
-        formRef.current.setErrors(errorMessages);
+          openModal();
+          setTimeout(function () {
+            history.push('/');
+          }, 3000)
+        } catch {
+          return setStatus({ type: 'error', mensagem: 'Erro: Este CPF já está cadastrado, volte para a tela de login.' });
+        }
       }
     }
-    history.push('/dashboard');
   }
 
-  return (
-    <>
-      <div className="menu-temp">
-        <a href="http://localhost:3000/new-participant">Participante</a>
-        <a href="http://localhost:3000/new-manager">Organizador</a>
-        <a href="http://localhost:3000/new-event">Evento</a>
-        <a href="http://localhost:3000/">Login</a>
-        <a href="http://localhost:3000/new-certificate">Gerar Certificado</a>
-        <a href="http://localhost:3000/manager">Dash Organizador</a>
-      </div>
-      <h1>CADASTRO DE NOVO USUÁRIO ORGANIZADOR/DOSCENTE</h1>
-      <div className="container">
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <label>Organizador</label>
-          <Input name="nome"
-            placeholder="Nome"
-            value={nome}
-            onChange={event => setNome(event.target.value)}
-          />
-          <Input name="sobrenome"
-            placeholder="Sobrenome"
-            value={sobrenome}
-            onChange={event => setSobrenome(event.target.value)}
-          />
-          <Input name="cpf"
-            placeholder="CPF"
-            value={cpf}
-            onChange={event => setCpf(event.target.value)}
-          />
-          <Input name="titulacao"
-            placeholder="Titulação"
-            value={titulacao}
-            onChange={event => setTitulacao(event.target.value)}
-          />
-          <Input name="cargo"
-            placeholder="Cargo"
-            value={cargo}
-            onChange={event => setCargo(event.target.value)}
-          />
-          <Input name="campus_instituicao"
-            placeholder="Campus Instituição"
-            value={campus_instituicao}
-            onChange={event => setCampus_instituicao(event.target.value)}
-          />
-          <label>Contato</label>
-          <Input name="telefone"
-            placeholder="Telefone"
-            value={telefone}
-            onChange={event => setTelefone(event.target.value)}
-          />
-          <Input name="email"
-            placeholder="E-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <Input name="password"
-            type= "password"
-            placeholder="Senha"
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
+  function validate() {
+    if (!nome) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo nome!' });
+    if (!sobrenome) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo sobrenome!' });
+    if (!cpf) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo CPF!' });
+    if (!campus_instituicao) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo campus/instituição!' });
+    if (!titulacao) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo titulação!' });
+    if (!cargo) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo cargo!' });
+    if (!telefone) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo telefone!' });
+    if (!email) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo email!' });
+    if (!password) return setStatus({ type: 'error', mensagem: 'Erro: Necessário preencher o campo senha!' });
+    if (!repeatPass) return setStatus({ type: 'error', mensagem: 'Erro: As senhas precisam ser iguais!' });
+    if (!repeatPass) return setStatus({ type: 'error', mensagem: 'Erro: Necessário repetir a senha!' });
+    if (repeatPass !== password) return setStatus({ type: 'error', mensagem: 'Erro: As senhas precisam ser iguais!' });
+    return true;
+  }
 
-          <button className="btn" type="submit">Cadastrar</button>
-        </Form>
-      </div>
-    </>
+
+  return (
+    <Container>
+      <ModalConfirm
+        showModal={showModal}
+        setShowModal={setShowModal}
+        message="Organizador cadastrado com sucesso!"
+      />
+      <Header back="true" />
+      <Content>
+
+        <form onSubmit={addUser}>
+          <ViewInputs>
+            <Input
+              label="Nome *"
+              type="text"
+              value={nome}
+              onChange={event => setNome(event.target.value)}
+            />
+
+            <Input
+              label="Sobrenome *"
+              type="text"
+              value={sobrenome}
+              onChange={event => setSobrenome(event.target.value)}
+            />
+
+            <Input
+              label="CPF * (apenas números)"
+              type="number"
+              value={cpf}
+              onChange={event => setCpf(event.target.value)}
+            />
+
+            <Input
+              label="Campus/Instituição *"
+              type="text"
+              value={campus_instituicao}
+              onChange={event => setCampus_instituicao(event.target.value)}
+            />
+
+            <Input
+              label="Titulação *"
+              type="text"
+              value={titulacao}
+              onChange={event => setTitulacao(event.target.value)}
+            />
+
+            <Input
+              label="Cargo *"
+              type="text"
+              value={cargo}
+              onChange={event => setCargo(event.target.value)}
+            />
+
+            <Input
+              label="Telefone *"
+              type="text"
+              value={telefone}
+              onChange={event => setTelefone(event.target.value)}
+            />
+
+            <Input
+              label="E-mail *"
+              type="email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+            />
+
+            <Input
+              label="Senha *"
+              type="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+            />
+
+            <Input
+              label="Repetir a Senha *"
+              type="password"
+              value={repeatPass}
+              onChange={event => setRepeatPass(event.target.value)}
+            />
+
+          </ViewInputs>
+          <ViewError>
+            {status.type === 'success' ? <p style={{ color: "green" }}>{status.mensagem}</p> : ""}
+            {status.type === 'error' ? <p style={{ color: "tomato" }}>{status.mensagem}</p> : ""}
+          </ViewError>
+          <View>
+            <Button name="Cadastrar" type="submit" />
+            <P>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <p>Voltar ao login</p>
+              </Link>
+            </P>
+          </View>
+        </form>
+      </Content>
+    </Container>
+
   )
 }
